adab154033635bdb655569f2c13df52590bd0aee
Caio Berdusco <devcaiosbs@gmail.com>
2026-07-14 02:01:20 -0300

    Implementation Material class

--- src/main/java/entity/Material.java
+++ src/main/java/entity/Material.java
@@ -1,5 +1,65 @@
 package entity;
 
-public class Material {
+import java.io.Serializable;
+import java.util.Objects;
 
+public class Material implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private final Long id;
+	private String name;
+	private String code;
+	private String plant;
+
+	public Material(Long id, String name, String code, String plant) {
+		this.id = id;
+		this.name = name;
+		this.code = code;
+		this.plant = plant;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public String getPlant() {
+		return plant;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public void setPlant(String plant) {
+		this.plant = plant;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Material other = (Material) obj;
+		return Objects.equals(id, other.id);
+	}
 }
